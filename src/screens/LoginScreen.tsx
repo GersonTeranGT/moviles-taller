73bf73177e6020701b3364c16d709c8029640535
GersonTeranGT <guapygt543@gmail.com>
2025-08-08 10:22:43 -0500

    cambios nulos

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -16,7 +16,7 @@ interface FormLogin {
 interface User {
     id: number;
     name: string;
-    tellphone: string;
+    phone: string;
     email: string;
     username: string;
     password: string;
@@ -24,8 +24,8 @@ interface User {
 
 //arreglo con la lista de usuarios
 const users: User[] = [
-    { id: 1, name: 'Gerson Teran', tellphone: '0984886447', email: 'guapygt543@gmail.com', username: 'GuapyGT', password: '12345' },
-    { id: 2, name: 'Michael Arévalo', tellphone: '0963268653', email: 'mba.arevalosabando@gmai.com', username: 'Mick', password: '67890' }
+    { id: 1, name: 'Gerson Teran', phone: '0984886447', email: 'guapygt543@gmail.com', username: 'GuapyGT', password: '12345' },
+    { id: 2, name: 'Michael Arévalo', phone: '0963268653', email: 'mba.arevalosabando@gmai.com', username: 'Mick', password: '67890' }
 ]
 export const LoginScreen = () => {
 
